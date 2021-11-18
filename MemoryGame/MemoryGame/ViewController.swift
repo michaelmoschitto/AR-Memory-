c9cc72106476a4cd5ab1c20022fd6e8b5b5b22ca
bella-tackle <bella@tackle.io>
2021-11-18 09:42:36 -0800

    initial reality kit app

---
+++ MemoryGame/MemoryGame/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by Isabella Dossola on 11/18/21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
---
+++ MemoryGame/MemoryGameUITests/MemoryGameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryGameUITestsLaunchTests.swift
+//  MemoryGameUITests
+//
+//  Created by Isabella Dossola on 11/18/21.
+//
+
+import XCTest
+
+class MemoryGameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
